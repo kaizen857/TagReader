31b77fbee3b8f68cb42657eb77c38446ac18a1bb
kaizen857 <3064561709@qq.com>
2026-04-24 15:10:17 +0800

    first commmit

---
+++ include/TagReader.hpp
@@ -0,0 +1,17 @@
+#ifndef __TAGREADER_HPP__
+#define __TAGREADER_HPP__
+
+#include <string>
+#include <string_view>
+
+struct Tag
+{
+    const std::string name;
+    const std::string value;
+};
+
+class TagReader
+{
+};
+
+#endif
